d5c37b8011a753e640ac4f97ce6fb2bb1ca31e4b
Juan <juan.manuel.iglesias93@gmail.com>
2018-04-05 18:29:21 -0300

    Arranca la aplicación

--- script.js
+++ script.js
@@ -5,8 +5,46 @@ $(document).ready(function(){
 function cargarEvaluaciones(){
 	$.getJSON("datos2.json",
 			function(data){
-				$.each(data, function() {
-        			$("ul").append("<li>Pepe</li>");
+				$.each(data.evaluaciones, function() {
+					//Parse de la fecha
+					var from = ($(this).attr("fecha")).split("/");
+					var f = new Date(from[2], from[1] - 1, from[0]);
+					var actual = new Date();
+					var clickeable;
+					//Si la fecha de la evaluación es anterior o igual a la actual entonces es seleccionable (clickeable)
+					if (f <= actual)
+						clickeable = true;
+					else clickeable = false;
+					//Se crea el identificador ev+ id de la evauluación
+					var identificacion = "ev"+$(this).attr("evaluacion");
+					//Se agrega la fila de la evaluación a la tabla 
+        			$("tbody").append("<tr id='"+identificacion+"'><td>"+$(this).attr("nombre")+"</td><td>"+$(this).attr("descripcion")+"</td><td>"+$(this).attr("fecha")+"</td></tr>");
+        			//Si es selecccionable entonces se agrega el atributo html clickeable como true y se agrega la función que se ejecuta al clickearla
+        			if (clickeable){ 
+        				$("#"+identificacion).attr('clickeable','true')
+        				$("#"+identificacion).click(function(){
+        					clickEvaluacion(identificacion);
+        				});
+        			}
     			});
 			});
+}
+
+function clickEvaluacion(identificacion){
+	if ( $("#"+identificacion).attr('clickeable') == "true"){
+		$("#"+identificacion).attr('clickeable','false');
+		
+		$.getJSON("datos2.json",
+				function(data){
+					$.each(data.evaluaciones_comisiones, function() {
+						if ("ev"+this.evaluacion == identificacion){
+							$("<table id='com"+this.comision+"'><tr><td>HOLA</td></tr></table>").insertAfter($("#"+identificacion));
+						}
+						
+	    			});
+				});
+	} else{
+		$("#com"+identificacion).closest('tr').next().remove();
+		$("#"+identificacion).attr('clickeable','true')
+	}
 }
