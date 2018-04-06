03d76f80bea8a0333b746c23a2917de82d8b7947
Juan <juan.manuel.iglesias93@gmail.com>
2018-04-06 12:07:56 -0300

    anda mejor mejor

--- script.js
+++ script.js
@@ -1,50 +1,90 @@
 $(document).ready(function(){
+	//window.localStorage.clear();
 	cargarEvaluaciones();
+
 });
 
 function cargarEvaluaciones(){
-	$.getJSON("datos2.json",
-			function(data){
-				$.each(data.evaluaciones, function() {
-					//Parse de la fecha
-					var from = ($(this).attr("fecha")).split("/");
-					var f = new Date(from[2], from[1] - 1, from[0]);
-					var actual = new Date();
-					var clickeable;
-					//Si la fecha de la evaluación es anterior o igual a la actual entonces es seleccionable (clickeable)
-					if (f <= actual)
-						clickeable = true;
-					else clickeable = false;
-					//Se crea el identificador ev+ id de la evauluación
-					var identificacion = "ev"+$(this).attr("evaluacion");
-					//Se agrega la fila de la evaluación a la tabla 
-        			$("tbody").append("<tr id='"+identificacion+"'><td>"+$(this).attr("nombre")+"</td><td>"+$(this).attr("descripcion")+"</td><td>"+$(this).attr("fecha")+"</td></tr>");
-        			//Si es selecccionable entonces se agrega el atributo html clickeable como true y se agrega la función que se ejecuta al clickearla
-        			if (clickeable){ 
-        				$("#"+identificacion).attr('clickeable','true')
-        				$("#"+identificacion).click(function(){
-        					clickEvaluacion(identificacion);
-        				});
-        			}
-    			});
-			});
+	if (window.localStorage.getItem("_datos") === null) {
+		$.getJSON("datos2.json",
+				function(data){
+					window.localStorage.setItem("_datos", JSON.stringify(data));
+					//Cargo las evaluaciones desde el JSON
+					cargarEvaluacionesAux(data.evaluaciones);		
+				});
+	} else {
+		//Cargo las evaluaciones desde el localStorage
+		var datos = JSON.parse(window.localStorage.getItem("_datos"));
+		cargarEvaluacionesAux(datos.evaluaciones);
+
+	}
+
+}
+
+function cargarEvaluacionesAux(evaluaciones){
+	//Carga las evaluaciones desde evaluaciones
+	$.each(evaluaciones, function() {
+						//Parse de la fecha
+						var from = ($(this).attr("fecha")).split("/");
+						var f = new Date(from[2], from[1] - 1, from[0]);
+						var actual = new Date();
+						var clickeable;
+						//Si la fecha de la evaluación es anterior o igual a la actual entonces es seleccionable (clickeable)
+						if (f <= actual)
+							clickeable = true;
+						else clickeable = false;
+						//Se crea el identificador ev+ id de la evauluación
+						var identificacion = "ev"+$(this).attr("evaluacion");
+						//Se agrega la fila de la evaluación a la tabla 
+	        			$("tbody").append("<tr id='"+identificacion+"'><td>"+$(this).attr("nombre")+"</td><td>"+$(this).attr("descripcion")+"</td><td>"+$(this).attr("fecha")+"</td></tr>");
+	        			//Si es selecccionable entonces se agrega el atributo html clickeable como true y se agrega la función que se ejecuta al clickearla
+	        			if (clickeable){ 
+	        				$("#"+identificacion).attr('clickeable','true');
+	        				$("#"+identificacion).attr('creada','false');
+	        				$("#"+identificacion).click(function(){
+	        					clickEvaluacion(identificacion);
+	        				});
+	        			}
+	   });
 }
 
 function clickEvaluacion(identificacion){
-	if ( $("#"+identificacion).attr('clickeable') == "true"){
+	if (( $("#"+identificacion).attr('clickeable') == "true") && ($("#"+identificacion).attr('creada') == "false")){
 		$("#"+identificacion).attr('clickeable','false');
-		
-		$.getJSON("datos2.json",
-				function(data){
-					$.each(data.evaluaciones_comisiones, function() {
-						if ("ev"+this.evaluacion == identificacion){
-							$("<table id='com"+this.comision+"'><tr><td>HOLA</td></tr></table>").insertAfter($("#"+identificacion));
-						}
-						
-	    			});
-				});
-	} else{
-		$("#com"+identificacion).closest('tr').next().remove();
-		$("#"+identificacion).attr('clickeable','true')
+		$("#"+identificacion).attr('creada','true');
+
+		var data = JSON.parse(window.localStorage.getItem("_datos"));
+
+		$.each(data.evaluaciones_comisiones, function() {
+			//Si la comisión corresponde a la evaluación entonces se agrega
+			if ("ev"+this.evaluacion == identificacion){
+				var completa = this.evaluacion_completa;
+				var nota = (completa == "true") ? "Nota: "+ this.nota +"</br>"+this.observaciones : "No Evaluado";
+				var comision = getElementArray(data.comisiones,"comision",this.comision);
+				$("<tr><td colspan='3'><table class='"+identificacion+"'><tr><td>"+comision.nombre+"</td></tr><tr><td>"+nota+"</td></tr></table></td></tr>").insertAfter($("#"+identificacion));
+			}
+				
+		});
+
+	} else if (( $("#"+identificacion).attr('clickeable') == "true") && ($("#"+identificacion).attr('creada') == "true")){
+		$("."+identificacion).show();
+		$("#"+identificacion).attr('clickeable','false');
+	} else {
+		$("."+identificacion).hide();
+		$("#"+identificacion).attr('clickeable','true');
 	}
+}
+
+function getElementArray(arreglo,elemento,valor){
+	var resultado = null;
+	var encontrado = false;
+	var i=0;
+	while (i < arreglo.length || !encontrado){
+		if (arreglo[i][elemento] == valor){
+			encontrado = true;
+			resultado = arreglo[i];
+		}
+		i++;
+	}
+	return resultado;
 }
